5a7cee394204c5583e57c3b1f5f5c5a4a3f05ee6
Lion2517 <lion2517@yahoo.com>
2025-06-15 21:57:16 +0500

    Полностью закончена вторая секция

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,9 @@
 import { initModalEvents } from './modal.js';
 import { initFormHandler } from './formHandler.js';
+import { initSlider } from './slider.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initModalEvents();
   initFormHandler();
+  initSlider();
 });
--- JS/slider.js
+++ JS/slider.js
@@ -1,46 +1,44 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const slider = document.querySelector('.slider');
+const slider = document.querySelector('.slider');
 
-    const slides = document.querySelectorAll('.slider__item');
-    const prevBtn = document.querySelector('.prev');
-    const nextBtn = document.querySelector('.next');
+const slides = document.querySelectorAll('.slider__item');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
 
-    let currentSlide = 0;
-    const slideCount = slides.length;
+let currentSlide = 0;
+const slideCount = slides.length;
 
-    function goToSlide(slideIndex) {
-        slider.style.transform = `translateX(-${slideIndex * 100}%)`;
-        currentSlide = slideIndex;
-    }
+function goToSlide(slideIndex) {
+    slider.style.transform = `translateX(-${slideIndex * 100}%)`;
+    currentSlide = slideIndex;
+}
 
-    function nextSlide() {
-        currentSlide = (currentSlide + 1) % slideCount;
-        goToSlide(currentSlide);
-    }
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slideCount;
+    goToSlide(currentSlide);
+}
 
-    function prevSlide() {
-        currentSlide = (currentSlide - 1 + slideCount) % slideCount;
-        goToSlide(currentSlide);
-    }
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slideCount) % slideCount;
+    goToSlide(currentSlide);
+}
 
-    nextBtn.addEventListener('click', nextSlide);
-    prevBtn.addEventListener('click', prevSlide);
+nextBtn.addEventListener('click', nextSlide);
+prevBtn.addEventListener('click', prevSlide);
 
-    // Свайп для мобильных устройств
-    let touchStartX = 0;
-    let touchEndX = 0;
+// Свайп для мобильных устройств
+let touchStartX = 0;
+let touchEndX = 0;
 
-    slider.addEventListener('touchstart', (e) => {
-        touchStartX = e.changedTouches[0].screenX;
-    }, { passive: true });
+slider.addEventListener('touchstart', (e) => {
+    touchStartX = e.changedTouches[0].screenX;
+}, { passive: true });
 
-    slider.addEventListener('touchend', (e) => {
-        touchEndX = e.changedTouches[0].screenX;
-        handleSwipe();
-    }, { passive: true });
+slider.addEventListener('touchend', (e) => {
+    touchEndX = e.changedTouches[0].screenX;
+    handleSwipe();
+}, { passive: true });
 
-    function handleSwipe() {
-        if (touchEndX < touchStartX - 50) nextSlide();
-        if (touchEndX > touchStartX + 50) prevSlide();
-    }
-});
+function handleSwipe() {
+    if (touchEndX < touchStartX - 50) nextSlide();
+    if (touchEndX > touchStartX + 50) prevSlide();
+}
