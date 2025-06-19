65a054682623d05c2d85b13497de357a58f3c8ee
Lion2517 <lion2517@yahoo.com>
2025-06-19 21:18:32 +0500

    Закончил последнюю секцию

--- JS/modal.js
+++ JS/modal.js
@@ -1,31 +1,28 @@
-const modal = document.getElementById('modal');
-const openBtns = document.querySelectorAll('.header__callback-button, .main__section--three__info--button');
-const closeBtn = modal.querySelector('.modal__close');
-const overlay = modal.querySelector('.modal__overlay');
-const body = document.body;
+  const modal = document.getElementById('modal');
+  const openBtns = document.querySelectorAll('.header__callback-button, .main__section--three__info--button');
+  const closeBtn = modal.querySelector('.modal__close');
+  const overlay = modal.querySelector('.modal__overlay');
+  const body = document.body;
 
-export function openModal() {
-  modal.classList.remove('modal--hidden');
-  modal.classList.add('modal--active');
-  body.classList.add('body--no-scroll');
-}
+  function openModal() {
+    modal.classList.remove('modal--hidden');
+    modal.classList.add('modal--active');
+    body.classList.add('body--no-scroll');
+  }
 
-export function closeModal() {
-  modal.classList.remove('modal--active');
-  modal.classList.add('modal--hidden');
-  body.classList.remove('body--no-scroll');
-}
+  function closeModal() {
+    modal.classList.remove('modal--active');
+    modal.classList.add('modal--hidden');
+    body.classList.remove('body--no-scroll');
+  }
 
-export function initModalEvents() {
-  // Добавляем обработчик для всех кнопок открытия
   openBtns.forEach(btn => {
     btn.addEventListener('click', openModal);
   });
-  
+
   closeBtn.addEventListener('click', closeModal);
   overlay.addEventListener('click', closeModal);
 
   document.addEventListener('keydown', (e) => {
     if (e.key === 'Escape') closeModal();
   });
-}
